5277c6c59ab494d7c808ffc84735ab45cd236107
Abdullah Alshoshan <alshoshanabdullah@gmail.com>
2019-02-23 12:17:24 +0300

    Fix some typos

--- part1.1.py
+++ part1.1.py
@@ -3,8 +3,8 @@ if __name__ == '__main__':
     f = open('message.txt', 'r')  # Open the file containing the message for read
     message = f.read()  # Read the message
     messageInByte = str.encode(message)  # Encode the message to bytes (the API accept text encoded in bytes)
-    hash = SHA512.new()  # Create hash object
-    hash.update(messageInByte)  # Compute the hash of the message
-    digest = hash.hexdigest()  # Retrieve the digest in hex
+    sha512Hash = SHA512.new()  # Create hash object
+    sha512Hash.update(messageInByte)  # Compute the hash of the message
+    digest = sha512Hash.hexdigest()  # Retrieve the digest in hex
     print('Original message:\n' + message + '\n')
     print('Digest in hex:\n' + digest)
